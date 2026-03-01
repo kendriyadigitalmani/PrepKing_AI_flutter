15494b69bc30d5dfdeb34871301a76c831a1bbc4
kendriyadigitalmani <kendriyadigital@gmail.com>
2026-03-01 10:47:28 +0530

    your message here

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // 🏷️ VERSION TRACKING - Update this when you deploy a new version
-const String appVersion = 'v2.1.1 mani';
+const String appVersion = 'v2.1.1 mani mani';
 const String buildDate = '01-Mar-2026';
 
 void main() { runApp(const PrepKingAIApp()); }
